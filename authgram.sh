fb77141cb61c3dcfc5ad6e6ea0c88830d2131e50
Goяo! <226759+gorobey@users.noreply.github.com>
2021-04-11 19:30:05 +0200

    Working Version
    Now is returned the effective key used for access

--- authgram.sh
+++ authgram.sh
@@ -8,28 +8,33 @@ USERID=""
 
 #bot key
 KEY=""
+URL=""
 
-TIMEOUT="10"
-URL="https://api.telegram.org/bot$KEY/sendMessage"
-AUTHLOG="/var/log/auth.log"
+AUTH_LOG="/var/log/auth.log"
 USER_INFO="$(ssh-keygen -l -f ~/.ssh/authorized_keys)"
-DATE_EXEC="$(date "+%d/%m-%Y %H:%M")"
-TMPINFO='/tmp/ipinfo-$DATE_EXEC.txt'
+DATE_EXEC="$(date "+%d/%m/%Y %H.%M.%I")"
+TMP_INFO='/tmp/ipinfo-$DATE_EXEC.txt'
+
 if [ -n "$SSH_CLIENT" ]; then
-        IP=$(echo $SSH_CLIENT | awk '{printf $1}')
-        PORT=$(echo $SSH_CLIENT | awk '{printf $3}')
-        HOSTNAME=$(hostname -f)
-        IPADDR=$(hostname -I | awk '{printf $1}')
-        curl https://ipinfo.io/$IP -s -o $TMPINFO
-        CITY=$(cat $TMPINFO | jq '.city' | sed 's/"//g')
-        REGION=$(cat $TMPINFO | jq '.region' | sed 's/"//g')
-        COUNTRY=$(cat $TMPINFO | jq '.country' | sed 's/"//g')
-read -r -d '' msg << EOM
-<b>$DATE_EXEC:</b>
-${USER}@$HOSTNAME:$PORT 
-$IP, $CITY, $REGION, $COUNTRY
-[ $USER_INFO ]
-EOM
-		curl --data chat_id="$USERID" --data-urlencode "text=${msg}" "https://api.telegram.org/bot$KEY/sendMessage?parse_mode=HTML" > /dev/null
-        rm $TMPINFO
+	IP=$(echo $SSH_CLIENT | awk '{printf $1}')
+	SSH_PROCESS=$(echo $SSH_CLIENT | awk '{printf $2}')
+	KEY_USED=$(cat $AUTH_LOG | grep $SSH_PROCESS | cut -f6- -d:)
+	KEY_INFO=$(ssh-keygen -l -f ~/.ssh/authorized_keys | grep $KEY_USED)
+	PORT=$(echo $SSH_CLIENT | awk '{printf $3}')
+	HOSTNAME=$(hostname -f)
+	IPADDR=$(hostname -I | awk '{printf $1}')
+	curl https://ipinfo.io/$IP -s -o $TMP_INFO
+	CITY=$(cat $TMP_INFO | jq '.city' | sed 's/"//g')
+	REGION=$(cat $TMP_INFO | jq '.region' | sed 's/"//g')
+	COUNTRY=$(cat $TMP_INFO | jq '.country' | sed 's/"//g')
+	read -r -d '' msg << EOM
+		<b>📅 $DATE_EXEC</b>
+		💻 ${USER} logged into $HOSTNAME : $PORT
+		From: $IP
+		🌎 GeoIP: $CITY, $REGION, $COUNTRY
+		🔑 Using Key:
+		BITS: $KEY_INFO
+	EOM
+	curl --data chat_id="$USERID" --data-urlencode "text=${msg}" "https://api.telegram.org/bot$KEY/sendMessage?parse_mode=HTML$
+	rm $TMP_INFO
 fi
